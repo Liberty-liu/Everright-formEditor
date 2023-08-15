3d4faa49183e8a49adc0da40f0f8f77b35f86883
Liberty <zhuizhuidea@gmail.com>
2023-08-15 17:04:27 +0800

    test(erGeneratorData): cellphone

--- test/erGeneratorData.test.js
+++ test/erGeneratorData.test.js
@@ -1,7 +1,10 @@
 import { describe, assert, expect, test, beforeEach } from 'vitest'
 import { erFormConfig, erGeneratorData, erComponentsConfig, utils } from '@ER/formEditor'
 describe('Generate field data:', () => {
-  test.only('Email', () => {
+  test('Email', () => {
     expect(erGeneratorData(erComponentsConfig.fieldsConfig[0].list[0], true, 'en')).toMatchSnapshot()
+  })
+  test('Cellphone', () => {
+    expect(erGeneratorData(erComponentsConfig.fieldsConfig[0].list[1], true, 'en')).toMatchSnapshot()
   })
 })
